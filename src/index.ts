c9488f26519bffdf23b77baf081495fe9595cdd8
vickybanti <olamuyiwavictor55@gmail.com>
2026-02-01 23:05:38 +0100

    changed subject import

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import AgentAPI from "apminsight";
 AgentAPI.config();
 
 import express from "express";
-import subjectRouter from './routes/subjects'
+import subjectRouter from './routes/subjects.js'
 import cors from "cors";
-import securityMiddleware from "./middleware/security";
+import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
 import {auth} from "./lib/auth";
 
