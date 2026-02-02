a32c014b4b67b8c97bdc633ad5c0e8969fb45980
vickybanti <olamuyiwavictor55@gmail.com>
2026-02-02 13:15:11 +0100

    changed all file extensions to js

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db"; // your drizzle instance
+import { db } from "../db/index.js"; // your drizzle instance
 import * as schema from "../db/schema/auth.js"
 
 export const auth = betterAuth({
